3a1041994fe939787b00df5ccb7f3f804213a706
Aftab Ahmad <aahmad@tribalscale.com>
2018-09-18 12:02:25 -0400

    chore: Add sample iOS project.

---
+++ Sample-IOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Sample-IOS
+//
+//  Created by Aftab Ahmad on 2018-09-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
